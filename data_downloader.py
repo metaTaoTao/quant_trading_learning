d7864984480fec90da56b990e87182d448afc257
Tao Zhang <uncczhangtao@gmail.com>
2024-02-01 21:18:19 -0500

    data

--- data_downloader.py
+++ data_downloader.py
@@ -1,7 +1,5 @@
 import yfinance as yf
 import pandas as pd
-data = yf.download('BTC-USD', start='2020-01-01', end='2024-01-30', interval='1d')
+data = yf.download('GMT-USD', start='2020-01-01', end='2024-01-30', interval='1d')
 
-data.to_csv('BTC-USD.csv')
-
-pd.rolling
+data.to_csv('BNB-USD.csv')
